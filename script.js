60daa754d29b167080a0508f2e13ba346ac6183e
angelia collins <angiecollins09@gmail.com>
2020-10-23 19:10:59 -0600

    refactor

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var QuestionsArr = [
     {
         question: "1. A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers:[
-            "JavaScript", 
-            "terminal/bash", 
+        answers: [
+            "JavaScript",
+            "terminal/bash",
             "for loops",
-            "console log", 
+            "console log",
         ],
-        correctAnswer: 3 
+        correctAnswer: 3
     },
     {
         question: "2. String values must be enclosed within ___ when being assigned to variables.",
@@ -17,7 +17,7 @@ var QuestionsArr = [
             "quotes",
             "parentheses",
         ],
-        correctAnswer: 2 
+        correctAnswer: 2
     },
     {
         question: "3. Arrays in JavaScript can be used to store _____.",
@@ -27,7 +27,7 @@ var QuestionsArr = [
             "booleans",
             "all of the above",
         ],
-        correctAnswer: 3 
+        correctAnswer: 3
     },
     {
         question: "4. The condition in an if / else statement is enclosed within _____.",
@@ -37,17 +37,17 @@ var QuestionsArr = [
             "parentheses",
             "square brackets",
         ],
-        correctAnswer: 2 
+        correctAnswer: 2
     },
     {
         question: "5. Commonly used data types DO NOT include:",
-        answers:[
-            "strings", 
-            "booleans", 
-            "alerts", 
+        answers: [
+            "strings",
+            "booleans",
+            "alerts",
             "numbers",
         ],
-        correctAnswer: 2 
+        correctAnswer: 2
     },
 ]
 var body = document.querySelector("body");
@@ -63,11 +63,11 @@ var idx_question = 0;
 var scoreCount = 0;
 
 
-function setCounterText(){
+function setCounterText() {
     timer.textContent = "Time: " + timeCount;
 }
 
-function startScreen(){
+function startScreen() {
     h1El.textContent = "It's time to take a quiz."
     pEl.textContent = "Take this Javascript quiz to stay nerdy. Your time will go down by 10 seconds for every wrong answer."
     startBtn.textContent = "Start Quiz"
@@ -85,41 +85,48 @@ startScreen();
 setCounterText();
 
 function makeTimer() {
-    var timerInterval = setInterval(function() {
-    timeCount--;
-    setCounterText(); 
-    idx_question++;
+    var timerInterval = setInterval(function () {
+        timeCount--;
+        setCounterText();
+        idx_question++;
 
-    if(timeCount === 0) {
-        clearInterval(timerInterval);
-        gameOver();
-    }
-}, 1000) 
+        if (timeCount === 0) {
+            clearInterval(timerInterval);
+            gameOver();
+        }
+    }, 1000)
 }
 
-startBtn.addEventListener("click", function(event) {
+startBtn.addEventListener("click", function (event) {
     event.preventDefault()
     startBtn.style.display = "none";
     answerDiv.style.display = "block";
     pEl.textContent = " ";
     // makeTimer();
-    h1El.textContent = QuestionsArr[idx_question].question;
-    
+    for (var idx_answer = 0; idx_answer < 4; idx_answer++) {
+        //Make Answer buttons clickable and recognize the right answer
+        var newAnswerBtn = document.getElementById("answer-" + idx_answer);
+        newAnswerBtn.addEventListener("click", function (answersEvent) {
+            var chosenAnswer = Number(this.id.substr(7));
+            var correctAnswer = QuestionsArr[idx_question].correctAnswer;
 
-    var answerQty = QuestionsArr[idx_question].answers.length;
+
+            alert(chosenAnswer === correctAnswer);
+        });
+    }
+    activateQuestion(0);
+});
+
+function activateQuestion(idx_new) {
+    h1El.textContent = QuestionsArr[idx_new].question;
+
+
+    var answerQty = QuestionsArr[idx_new].answers.length;
 
 
     for (var idx_answer = 0; idx_answer < answerQty; idx_answer++) {
         var newAnswerBtn = document.getElementById("answer-" + idx_answer);
-        newAnswerBtn.textContent = QuestionsArr[idx_question].answers[idx_answer];
-        
-        //Make Answer buttons clickable and recognize the right answer
-        newAnswerBtn.addEventListener("click", function(answersEvent) {
-            var chosenAnswer = Number(this.id.substr(7));
-            var correctAnswer = QuestionsArr[idx_question].correctAnswer;
-        
-
-            alert(chosenAnswer === correctAnswer);
-        });
-        }
-    });
+        newAnswerBtn.textContent = QuestionsArr[idx_new].answers[idx_answer];
+    }
+    idx_question = idx_new;
+}
